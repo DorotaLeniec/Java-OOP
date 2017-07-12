dd4b3f5df700bfc3705b2793cdcd527ae765a236
Dorota Leniec <dorleniec@gmail.com>
2017-07-12 20:12:08 +0200

    stworzenie klasy Person

---
+++ src/Zadania2/Person.java
@@ -0,0 +1,43 @@
+package Zadania2;
+
+/**
+ * Created by dorka on 12.07.2017.
+ */
+public class Person {
+    private String name;
+    private String surName;
+    private int age;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Person(String name, String surname, int age){
+        this.name = name;
+        this.surName = surname;
+        this.age = age;
+    }
+
+
+}
