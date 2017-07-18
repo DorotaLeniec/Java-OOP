b1b6295609bbc7afdc63182e7a5651469b1228a5
Dorota Leniec <dorleniec@gmail.com>
2017-07-18 19:39:24 +0200

    Wykorzystanie metody Integer.compare

--- src/collections/list/queue/AgeComparator.java
+++ src/collections/list/queue/AgeComparator.java
@@ -9,6 +9,7 @@ public class AgeComparator implements Comparator<Person>{
 
     @Override
     public int compare(Person o1, Person o2) {
-        return o1.getAge() - o2.getAge();
+        return Integer.compare(o1.getAge(),o2.getAge());
+//        return o1.getAge() - o2.getAge();
     }
 }
