3bcb14d2e7fdc00349740671696a5f5259bb58b7
Dorota Leniec <dorleniec@gmail.com>
2017-07-18 19:30:32 +0200

    rozszerzenie klasy o interfejs Comparable

---
+++ src/collections/list/queue/Person.java
@@ -0,0 +1,29 @@
+package collections.list.queue;
+
+import java.util.Comparator;
+
+/**
+ * Created by dorka on 18.07.2017.
+ */
+public class Person implements Comparable<Person>{
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public int compareTo(Person other) {
+        return  other.age - this.age;
+    }
+}
