fa17c22195ae1b594f1fe40615cf1beeb9ea269d
DorotaLeniec <dorleniec@gmail.com>
2017-07-22 15:14:03 +0200

    generator losowych znaków

---
+++ src/generator/CharGenerator.java
@@ -0,0 +1,20 @@
+package generator;
+
+/**
+ * Created by RENT on 2017-07-22.
+ */
+public class CharGenerator {
+    RandomNumbersGenerator randomCharNumbers = new RandomNumbersGenerator('a','z');
+
+    public char generate(){
+        return (char)randomCharNumbers.generate();
+    }
+
+    public static void main(String[] args) {
+        CharGenerator charGenerator = new CharGenerator();
+        for(int i=0;i<26;i++){
+            System.out.println(charGenerator.generate());
+        }
+    }
+
+}
